dcd6a72c050135e157723a1b136dabb7e37ddda2
xiaohehu <xiaohe.hu@macminiveloper1.neoscape.local>
2014-06-10 17:10:20 -0400

    Initial Commit

---
+++ xh_galleryOverlay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  xh_galleryOverlay
+//
+//  Created by Xiaohe Hu on 6/10/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ xh_galleryOverlayTests/xh_galleryOverlayTests.m
@@ -0,0 +1,34 @@
+//
+//  xh_galleryOverlayTests.m
+//  xh_galleryOverlayTests
+//
+//  Created by Xiaohe Hu on 6/10/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xh_galleryOverlayTests : XCTestCase
+
+@end
+
+@implementation xh_galleryOverlayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
